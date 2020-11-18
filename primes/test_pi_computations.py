def6c8917e44c84738739c6c0b4acf4a9efcdb96
mateusz <matteusz.lichota@gmail.com>
2020-11-18 15:55:29 +0000

    add tests for pi computation methods

--- primes/test_pi_computations.py
+++ primes/test_pi_computations.py
@@ -2,6 +2,7 @@ import unittest
 import math
 
 import primes
+import primes.pi_computations as PC
 
 class TestS(unittest.TestCase):
     def setUp(self) -> None:
@@ -11,7 +12,7 @@ class TestS(unittest.TestCase):
     def test_S_equal_S_brute_force(self):
         pl = primes.prime_list(prime_sieve=self.ps, upto=math.isqrt(self.N))
         for k in range(0,4):
-            self.assertEqual(primes.S(self.N, k, pl), primes.S_brute_force(self.N, k, pl), msg="n={}, k={}".format(self.N, k)) 
+            self.assertEqual(PC.S(self.N, k, pl), PC.S_brute_force(self.N, k, pl), msg="n={}, k={}".format(self.N, k)) 
 
 class TestLegendreSum(unittest.TestCase):
     def setUp(self) -> None:
@@ -23,6 +24,26 @@ class TestLegendreSum(unittest.TestCase):
         pl = primes.prime_list(prime_sieve=self.ps, upto=self.N)
         self.assertTrue(True)
         
+class TestPiComputationMethods(unittest.TestCase):
+    def setUp(self) -> None:
+        self.N = 10**5
+        self.test_values = zip(range(0, 6), (0, 4, 25, 168, 1229, 9592))
+
+    def test_pi_from_list(self):
+        ps = primes.PrimeSieve(self.N)
+        for n, correct_val in self.test_values:
+            res = PC.pi_from_list(10**n, prime_list=primes.prime_list(prime_sieve=ps, upto=10**n))
+            self.assertEqual(res, correct_val)
+
+    def test_pi_meissel(self):
+        for n, correct_val in self.test_values:
+            res = PC.pi_meissel(n)
+            self.assertEqual(res, correct_val)
+
+    def test_pi_lucy_hedgehog(self):
+        for n, correct_val in self.test_values:
+            res = PC.pi_lucy_hedgehog(n)
+            self.assertEqual(res, correct_val)
 
 if __name__ == "__main__":
     unittest.main()
