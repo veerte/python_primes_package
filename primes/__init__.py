09c9c3b8ed6830be90976b9f4ed310bbb8e4168b
mateusz <matteusz.lichota@gmail.com>
2020-11-18 15:54:19 +0000

    change __init__.py to not import everything from pi_computations

--- primes/__init__.py
+++ primes/__init__.py
@@ -1,4 +1,4 @@
 from .primes_utils import *
 from .primeSieve import *
 from .factorization import *
-from .pi_computations import *
+from .pi_computations import pi_brute, pi_legendre, pi_meissel, pi_lucy_hedgehog
