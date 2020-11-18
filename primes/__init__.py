ef332fdc7ff9878fab6700b4f8f9569394c26a89
mateusz <matteusz.lichota@gmail.com>
2020-11-18 22:03:44 +0000

    make __init__.py import everything explicitly

--- primes/__init__.py
+++ primes/__init__.py
@@ -1,4 +1,4 @@
-from .primes_utils import *
-from .primeSieve import *
-from .factorization import *
+from .primes_utils import check_prime, next_prime, prime_list, nth_prime
+from .primeSieve import PrimeSieve
+from .factorization import get_prime_factors, prime_factors_generator
 from .pi_computations import pi_brute, pi_legendre, pi_meissel, pi_lucy_hedgehog
